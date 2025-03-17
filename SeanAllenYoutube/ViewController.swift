cd2637860c65a4441b64e62680eedcf176030974
hamidkrml <karimlihamid4@gmail.com>
2025-03-17 22:21:23 +0300

    1

--- SeanAllenYoutube/ViewController.swift
+++ SeanAllenYoutube/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     var loginButton :UIButton = {
+        let button = UIButton(type: .system)
+         button.backgroundColor = .systemBlue
+         button.layer.cornerRadius = 5
+         button.setTitle("hamit", for: .normal)
+         button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+        buttonconfigure()
     }
 
+    
+    func buttonconfigure(){
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginButton.widthAnchor.constraint(equalToConstant: 150),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
 
 }
 
+#Preview{
+    ViewController()
+}
