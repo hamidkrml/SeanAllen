74cb9e320619c6a15c3fb101a644d865e0d56e0d
hamidkrml <karimlihamid4@gmail.com>
2025-03-17 23:15:36 +0300

    tabbar aded

--- SeanAllenYoutube/SceneDelegate.swift
+++ SeanAllenYoutube/SceneDelegate.swift
@@ -16,14 +16,42 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
        
         guard let windowScane = (scene as? UIWindowScene) else { return }
         
+        
+        
+        let searchNC = UINavigationController(rootViewController: SearchVC())
+        let favoriteNC = UINavigationController(rootViewController: FavoriteVC())
+
+        let tabbar  = UITabBarController()
+        tabbar.viewControllers = [searchNC,favoriteNC]
+        
+        
         window = UIWindow(frame: windowScane.coordinateSpace.bounds)
         window?.windowScene = windowScane
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = tabbar
         window?.makeKeyAndVisible()
         
     }
 
-   
+   func createSearchNavigationControler() -> UINavigationController {
+       let searchVC = SearchVC()
+       searchVC.title = "Search"
+       searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+       
+       return  UINavigationController(rootViewController: searchVC)
+    }
 
+    
+    
+    func createFavoriteNavigationControler() -> UINavigationController {
+        let favoritelistVC = FavoriteVC()
+        favoritelistVC.title = "Favorites"
+        favoritelistVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return  UINavigationController(rootViewController: favoritelistVC)
+    }
+    
+    func createTabbar(){
+        
+    }
 }
 
---
+++ SeanAllenYoutube/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  SeanAllenYoutube
+//
+//  Created by hamid on 17.03.25.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+    
+
+  
+
+}
